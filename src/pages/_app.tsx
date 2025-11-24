13ea206757c81d99a32a56a9a3a1ec3ae646005f
Czeh0210 <chuiitan0210@gmail.com>
2025-11-24 17:56:16 +0800

    Update global styles and rename CSS file for consistency; replace 'global.css' with 'globals.css' in imports and configuration.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app';
-import '../styles/global.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
