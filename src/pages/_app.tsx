1680056197ff854238709fb8e2c848eff6a9f9a3
wwaiyyee <wwaiyyee@gmail.com>
2025-11-24 22:38:20 +0800

    Wrap application in WalletProvider for wallet integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { WalletProvider } from '../../providers/WalletProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
+  );
 }
